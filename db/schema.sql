99ca17af910b3c1a54e11ce3b40630d23378b504
Merel Jacobs <merel.burleigh@gmail.com>
2023-07-26 16:33:47 -0700

    departments & sql functional

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,14 @@ DROP DATABASE IF EXISTS USAFinancial_db;
 CREATE DATABASE USAFinancial_db;
 
 USE USAFinancial_db;
+
+CREATE TABLE departments (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+INSERT INTO departments (id, name) 
+VALUES 
+    ( 1, 'Legal'),
+    ( 2, 'Engineering'),
+    ( 3, 'Investments');
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
+// no variable necessary
+require('dotenv').config();
 
 
-const connection = mysql.createPool({
+
+const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: process.env.PASSWORDMYSQL,
-    database: 'USAFinancial_db'
+    database: 'USAFinancial_db',
+    socketPath: '/tmp/mysql.sock'
 });
 
 const questions = [{
@@ -16,15 +20,20 @@ const questions = [{
     choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Update an employee role']
 }]
 
+const questions2 = [{
+    type: 'list',
+    name: 'secondaryActions',
+    message: 'What would you like to do next?',
+    choices: ['View Departments', 'Add Another Department', 'Main Menu']
+}]
 
-
+// combination function
 function init() {
     inquirer.prompt(questions)
     .then((answer) => {
-        console.log(answer);
         switch (answer.action) {
             case 'View all departments':
-                console.log('department');
+                viewAllDepartments();
                 break;
             case 'View all roles':
                 console.log('roles');
@@ -33,7 +42,7 @@ function init() {
                 console.log('roles');
                 break;
             case 'Add a department':
-                console.log('roles');
+                addDepartment();
                 break;
             case 'Add a role':
                 console.log('roles');
@@ -45,7 +54,49 @@ function init() {
                 console.log('Invalid choice');
                 break;
         }})
-        .catch((err) => console.err('Error occurred: ', err))
+        .catch((err) => console.error('Error occurred: ', err))
 }
 
+//each function 
+function viewAllDepartments() {
+    connection.query(`SELECT * FROM departments`, (err, res) => {
+        if (err) throw err;
+        console.log('Viewing all Departments:');
+        console.table(res);
+        inquirer.prompt(questions2)
+        .then((answer) => {
+            switch (answer.secondaryActions) {
+                case 'Main Menu':
+                    init();
+                break;
+            }
+        })
+
+    });
+}
+
+function addDepartment() {
+    inquirer.prompt([{
+        type: 'input',
+        name: 'addNameOfDepartment',
+        message: 'What is the new department called?'
+    }])
+    .then((answer) => {
+        connection.query(`INSERT INTO departments (name) VALUES ('${answer.addNameOfDepartment}')`)})
+    .then(() => inquirer.prompt(questions2))
+    .then((answer) => {
+        switch (answer.secondaryActions) {
+            case 'View Departments':
+                viewAllDepartments();
+                break;
+            case 'Add Another Department':
+                addDepartment();
+                break;
+            case 'Main Menu':
+                init();
+                break;
+        }
+    });
+
+}
 init();
